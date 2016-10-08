cc31a97ee296f3bbaa79ab1194c4782e274c5dc0
Carlos Casares <ccasares@gmail.com>
2016-10-08 15:35:31 +0200

    new command

--- lcdDisplay.py
+++ lcdDisplay.py
@@ -312,7 +312,6 @@ def handleButton(button, screen, event):
 	  cad.lcd.set_cursor(0, 1)
 	  cad.lcd.write("IOT PROXY...")
 	  run_cmd(RESET_IOTPROXY_CMD)
-	  displayInfoRotation(event.chip)
     if button == BUTTON1:
 	  buttonWaitingForConfirmation = button
 	  cad.lcd.clear()
